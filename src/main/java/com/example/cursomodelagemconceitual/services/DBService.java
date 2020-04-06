15d894eb69a8a47a01d231db15b8a959f3dbd774
Edberto Leonardo <edbleonardo@gmail.com>
2020-04-06 13:51:58 -0300

    Implementando SmtpEmailService comservidor do Google(só pra lembrar o postman da erro 500 Username and Password not accepted)

--- src/main/java/com/example/cursomodelagemconceitual/services/DBService.java
+++ src/main/java/com/example/cursomodelagemconceitual/services/DBService.java
@@ -112,7 +112,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "edbleonardo@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "993838393"));
 		
